c112b45604d415572e48364c9b2e37a586c12438
陳姿穎 <roeyang1410@gmail.com>
2018-01-15 23:43:54 +0800

    修正MailController

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Mail;
 class MailController extends Controller
 {
     public function getSend()
@@ -12,8 +12,10 @@ class MailController extends Controller
          $data = ['name' => '張三', 'content' => $content,];
          Mail::send('email.test', $data, function ($message) {
                 $message->subject('Laravel 5 Mail');
-                $message->to('poikl15397@gmail.com', '張三');
+                $message->to('roeyang1410@gmail.com', '張三');
                 $message->from('netadmin@ncut.edu.tw', 'netadmin');
             });
+
+         return "Email已寄出";
      }
 }
