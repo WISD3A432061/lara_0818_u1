29022ce71b0c2df6eb93fef1139a6c1ac0d297db
陳姿穎 <roeyang1410@gmail.com>
2018-01-15 22:47:17 +0800

    測試middleware

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,10 +9,11 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Http\Middleware\CheckAge;
 
 Route::get('/', function () {
     return view('welcome');
-})->middleware('first', 'second');
+})->middleware(CheckAge::class);
 
 Route::get('/tracy', function () {
     throw new \Exception('Tracy works!');
