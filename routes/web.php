fe85534e94543223f695e535bcb5bdcfeacc4e20
陳姿穎 <roeyang1410@gmail.com>
2018-01-15 22:42:14 +0800

    下身份驗證命令

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Route::get('/tracy', function () {
     throw new \Exception('Tracy works!');
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
